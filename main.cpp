4d6c190a74dcf3265411dff10a12f6739fe635e4
norkella <norkella28@gmail.com>
2026-07-27 19:27:36 +0200

    setup

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include "bomberman/sfml/Game.h"
+
+#include <exception>
+#include <iostream>
+
+int main()
+{
+    try {
+        bomberman::sfml::Game game;
+        game.run();
+    } catch (const std::exception& exception) {
+        std::cerr << exception.what() << '\n';
+        return 1;
+    }
+
+    return 0;
+}
